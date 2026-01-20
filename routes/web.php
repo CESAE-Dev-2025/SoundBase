2f7f2a7537d9433429010493641b952d0e2f4e65
Leandro Gabriel <leandro@bigiogabriel.com>
2026-01-20 12:46:58 +0000

    Cria Controller utilitário para rota de fallback

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UtilController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ------------------------------------------------------------------- Fallback
+Route::fallback([UtilController::class, 'fallback']);
