d658bdfe9d5c75c141894d428878fb7f0a0745b9
Leandro Gabriel <leandro@bigiogabriel.com>
2026-01-20 12:48:28 +0000

    Adiciona albuns

---
+++ database/migrations/2026_01_20_123817_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('photo')->nullable();
+            $table->date('release_date')->nullable();
+            $table->unsignedBigInteger('band_id');
+            $table->timestamps();
+
+            $table->foreign('band_id')->references('id')->on('bands')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
